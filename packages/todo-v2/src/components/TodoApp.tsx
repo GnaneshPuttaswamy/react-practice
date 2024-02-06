255aa4c4ccdace4fde4a77b565850bcc3cc8b023
Gnanesh Puttaswamy <puttadt@gmail.com>
2024-02-06 14:47:47 +0530

    v2 - implemented custom hook useTodoState

--- packages/todo-v2/src/components/TodoApp.tsx
+++ packages/todo-v2/src/components/TodoApp.tsx
@@ -1,9 +1,9 @@
-import React, { CSSProperties, useState } from "react";
+import React, { CSSProperties } from "react";
 import { Layout } from "antd";
 import TodoInputForm from "./TodoInputForm";
 import TodoList from "./TodoList";
 const { Header, Content } = Layout;
-import { v4 as uuidv4 } from "uuid";
+import useTodoState from "../customHooks/useTodoState";
 
 const layoutStyle: CSSProperties = {
   width: "100%",
@@ -27,70 +27,8 @@ export interface Todo {
   isCompleted: boolean;
 }
 
-const initialTodos: Todo[] = [
-  {
-    id: "1",
-    title: "Todo 1",
-    isCompleted: false,
-  },
-  {
-    id: "2",
-    title: "Todo 2",
-    isCompleted: true,
-  },
-  {
-    id: "3",
-    title: "Todo 3",
-    isCompleted: true,
-  },
-  {
-    id: "4",
-    title: "Todo 4",
-    isCompleted: false,
-  },
-];
-
 const TodoApp: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>(initialTodos);
-
-  const addTodo = (title: string): void => {
-    const newTodo: Todo = {
-      id: uuidv4(),
-      title,
-      isCompleted: false,
-    };
-
-    setTodos([...todos, newTodo]);
-  };
-
-  const toggleTodo = (id: string): void => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: !todo.isCompleted };
-      }
-
-      return { ...todo };
-    });
-
-    setTodos(updatedTodos);
-  };
-
-  const editTodo = (id: string, title: string): void => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, title };
-      }
-
-      return { ...todo };
-    });
-
-    setTodos(updatedTodos);
-  };
-
-  const deleteTodo = (id: string): void => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
+  const { todos, addTodo, toggleTodo, editTodo, deleteTodo } = useTodoState();
 
   return (
     <Layout style={layoutStyle}>
--- packages/todo-v2/src/components/TodoItem.tsx
+++ packages/todo-v2/src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Button, Checkbox, Form, Input, List, Typography } from "antd";
 import {
   EditOutlined,
